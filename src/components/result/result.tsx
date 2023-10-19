7f41ac24f538e19aed992ed8821e2b88e833e6c5
R2u1s <Budz@yandex.ru>
2023-10-19 12:07:03 +0300

    добавить лоадер при загрузке контента

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './result.module.css';
+import { DELAY_RESULT_SHOW } from '../../constants/delays';
+import { Loader } from '../loader/loader';
 
 export const Result: React.FC = () => {
+
+  const [loaderState, setLoaderState] = React.useState<boolean>(true); //состояние, описывающее надо ли показывать результат
+
+  setTimeout(() => {       // задержка для работы лоадера
+    setLoaderState(false);
+  }, DELAY_RESULT_SHOW*5);
+
   return (
     <>
       <section className={`${styles.result}`}>
-        Result
+        {loaderState ? <Loader/> : <p>Result</p>}
       </section>
     </>
   );
