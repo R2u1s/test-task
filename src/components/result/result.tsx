f159ea02ee939c59b32171945e6bcbf10923ecb5
R2u1s <Budz@yandex.ru>
2023-11-01 18:14:19 +0300

    реализовать работу лоадера во время загрузки книг

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -3,22 +3,15 @@ import styles from './result.module.css';
 import { useSelector, useDispatch } from '../../services/hooks';
 import { DELAY_RESULT_SHOW } from '../../constants/delays';
 import { Loader } from '../loader/loader';
-import { bookInfoMock, bookListMock } from '../../constants/mock';
 import { Card } from '../card/card';
 
 export const Result: React.FC = () => {
-
-  const [loaderState, setLoaderState] = React.useState<boolean>(true); //состояние, описывающее надо ли показывать лоадер, потом заменить на стэйт из хранилища
 
   const { books } = useSelector((store) => ({
     books: store.books,
     searchRequest: store.books.searchRequest,
     searchFailed: store.books.searchFailed,
   }));
-
-  setTimeout(() => {       // задержка для работы лоадера
-    setLoaderState(false);
-  }, DELAY_RESULT_SHOW * 1);
 
   return (
     <>
